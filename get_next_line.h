cab137e56daff8ed15551f9329013df07bb3aef4
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-11 18:42:53 +0200

    add empty mandatory files

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 16:06:06 by estettle          #+#    #+#             */
+/*   Updated: 2024/10/11 16:18:26 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// libraries
+# include <stdlib.h> // for malloc() and free()
+# include <unistd.h> // for read()
+
+#endif
