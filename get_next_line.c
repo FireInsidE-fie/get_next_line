7950d8da0d841741244fc8163eb47d3d35428b24
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-14 10:44:57 +0200

    fix a few errors and start testing full file reads

--- get_next_line.c
+++ get_next_line.c
@@ -18,13 +18,13 @@ char	*get_next_line(int fd)
 	char	*buffer;
 
 	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	if (!buffer)
+		return (NULL);
 	read(fd, buffer, 1);
 	i = 0;
-	while (buffer[i] != EOF && buffer[i] != '\n' && i < BUFFER_SIZE)
-	{
-		i++;
-		read(fd, buffer + i, 1);
-	}
+	while (buffer[i] != EOF && buffer[i] != '\n' && ++i < BUFFER_SIZE)
+		if (!read(fd, buffer + i, 1))
+			return (NULL);
 	return (buffer);
 }
 
@@ -34,17 +34,26 @@ char	*get_next_line(int fd)
 // don't forget to always include a single (or two!) file path, silly
 int	main(int argc, char **argv)
 {
-	int	fd1;
-	int	fd2;
+	int		fd1;
+	//int		fd2;
+	char	*str;
 
-	if (argc != 3)
+	if (argc != 2)
 		return (0);
 	fd1 = open(argv[1], O_RDONLY);
-	fd2 = open(argv[2], O_RDONLY);
+	//fd2 = open(argv[2], O_RDONLY);
+	/*
 	printf("%s", get_next_line(fd1));
 	printf("%s", get_next_line(fd2));
 	printf("%s", get_next_line(fd1));
 	printf("%s", get_next_line(fd2));
+	*/
+	str = get_next_line(fd1);
+	while (*str)
+	{
+		printf("%s", str);
+		str = get_next_line(fd1);
+	}
 	close(fd1);
-	close(fd2);
+	//close(fd2);
 }
